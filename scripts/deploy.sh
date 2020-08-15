e09c0a669634f9dc45d0424d20013b054eec40d8
Leonardo Lima <leonardoaugusto287@gmail.com>
2020-08-14 21:25:47 -0300

    Deploy script enhancements and update README

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-# Installing AWS CLI
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
-# Installing ECS CLI
-sudo curl -Lo /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest
-gpg --keyserver hkp://keys.gnupg.net --recv BCE9D9A42D51784F
-sudo chmod +x /usr/local/bin/ecs-cli
-
-# AWS Login
-ecs-cli configure profile --profile-name profile --access-key $AWS_ACCESS_KEY_ID --secret-key $AWS_SECRET_ACCESS_KEY
-
 # login DockerHub
 docker login --username $DOCKER_HUB_USER --password $DOCKER_HUB_PASS
 
 # Push Dockerfile to DockerHub
-docker build -t $IMAGE_REPO_URL .
-docker tag $IMAGE_REPO_URL:latest $IMAGE_REPO_URL:latest
-docker push $IMAGE_REPO_URL:latest
+docker-compose build --pull
+docker-compose push
 
-# Deleting ECS Task
-TASK_ID=`aws ecs list-tasks --desired-status "RUNNING" --cluster $ECS_CLUSTER_NAME | grep arn | sed 's/[ "]//g'`
-aws ecs stop-task --cluster $ECS_CLUSTER_NAME --task $TASK_ID --reason "Deploy" > /dev/null
+# Installing AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip > /dev/null
+sudo ./aws/install
 
-# Creating ECS Task
-ecs-cli compose --project-name $ECS_CLUSTER_NAME --task-role-arn arn:aws:iam::816898588873:role/ecsTaskExecutionRole \ 
-    service create --cluster-config $ECS_CLUSTER_NAME
+# Login AWS
+aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+aws configure set region $AWS_DEFAULT_REGION
 
-exit 0
+# Update service
+aws ecs update-service --cluster $ECS_CLUSTER_NAME --service $ECS_SERVICE_NAME --force-new-deployment
